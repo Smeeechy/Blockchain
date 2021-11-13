2a4118541ad500b9122822ebfc0fb5a6cc2f5e5a
darynsmith <d.maccoy.smith@gmail.com>
2021-11-12 23:52:56 -0700

    Added serialization, saving to and loading from files

--- src/smeechy/Blockchain.java
+++ src/smeechy/Blockchain.java
@@ -1,5 +1,6 @@
 package smeechy;
 
+import java.io.Serializable;
 import java.security.MessageDigest;
 import java.time.Duration;
 import java.time.LocalTime;
@@ -7,7 +8,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
 
-public final class Blockchain {
+public final class Blockchain implements Serializable {
+    private final long serialVersionUID = 1L;
     private final ArrayList<Block> blocks;
     private final int zeros;
 
@@ -69,6 +71,18 @@ public final class Blockchain {
         return true;
     }
 
+    /**
+     *
+     * @return  the first five {@code Block}s in this {@code Blockchain}
+     */
+    public String preview() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 5; i++) {
+            builder.append(this.blocks.get(i));
+        }
+        return builder.toString();
+    }
+
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
@@ -79,16 +93,29 @@ public final class Blockchain {
     }
 }
 
-final class Block {
+final class Block implements Serializable {
+    private final long serialVersionUID = 1L;
     private final long id;
     private final long timestamp;
     private final String previousHash;
     private int magic;
 
+    /**
+     * Instantiates a new {@code Block} with specified {@code id} and {@code previousHash}. Sets {@code magic} to
+     * default of 0.
+     * @param id            this {@code Block}'s unique identifier.
+     * @param previousHash  a reference to the hash of the last proved {@code Block} in the {@code Blockchain}.
+     */
     public Block(long id, String previousHash) {
         this(id, previousHash, 0);
     }
 
+    /**
+     * Instantiates a new {@code Block} with specified {@code id}, {@code previousHash}, and {@code magic}.
+     * @param id            this {@code Block}'s unique identifier.
+     * @param previousHash  a reference to the hash of the last proved {@code Block} in the {@code Blockchain}.
+     * @param magic         used to generate a valid hash for this {@code Block}.
+     */
     public Block(long id, String previousHash, int magic) {
         this.id = id;
         this.timestamp = new Date().getTime();
@@ -96,14 +123,22 @@ final class Block {
         this.magic = magic;
     }
 
-    public String generateHash() {
-        return StringUtil.applySha256(this.id + this.timestamp + this.previousHash + this.magic);
-    }
-
     public String getPreviousHash() {
         return this.previousHash;
     }
 
+    /**
+     * Uses SHA-256 encryption.
+     * @return  a hash from all of the {@code Block}'s current field values.
+     */
+    public String generateHash() {
+        return StringUtil.applySha256(this.id + this.timestamp + this.previousHash + this.magic);
+    }
+
+    /**
+     * Used by {@code Blockchain} while generating a valid {@code Block}.
+     * @param magic the new value of the {@code Block}'s magic number.
+     */
     public void setMagic(int magic) {
         this.magic = magic;
     }
@@ -113,6 +148,7 @@ final class Block {
         StringBuilder builder = new StringBuilder("Block:\n");
         builder.append("Id: ").append(this.id).append("\n");
         builder.append("Timestamp: ").append(this.timestamp).append("\n");
+        builder.append("Magic number: ").append(this.magic).append("\n");
         builder.append("Hash of the previous block: \n").append(this.previousHash).append("\n");
         builder.append("Hash of the block: \n").append(this.generateHash());
         return builder.toString();
--- src/smeechy/Main.java
+++ src/smeechy/Main.java
@@ -1,15 +1,72 @@
 package smeechy;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final File file = new File("/Users/darynsmith/IdeaProjects/BlockchainTest/data/blockchain.bin");
+    private static Blockchain blockchain;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        if (loadBlockchain()) {
+            if (blockchain.validate()) {
+                System.out.println("Valid blockchain loaded from file.");
+            } else {
+                System.out.println("Blockchain loaded from file is invalid.");
+                createNewBlockchain();
+            }
+        } else {
+            System.out.println("Unable to load blockchain from file.");
+            createNewBlockchain();
+        }
+        generateNBlocks(5);
+        System.out.println(blockchain.preview());
+    }
+
+    private static void createNewBlockchain() {
         System.out.print("Enter how many zeros the hash must start with: ");
         int zeros = scanner.nextInt();
-        Blockchain dogecoin = new Blockchain(zeros);
-        while (true) {
-            dogecoin.generateBlock();
+        blockchain = new Blockchain(zeros);
+    }
+
+    private static void generateNBlocks(int n) {
+        for (int i = 0; i < n; i++) {
+            blockchain.generateBlock();
+            saveBlockchain();
         }
+    }
+
+    public static boolean saveBlockchain() {
+        try (FileOutputStream fos = new FileOutputStream(file);
+             BufferedOutputStream bos = new BufferedOutputStream(fos);
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            oos.writeObject(blockchain);
+            return true;
+        } catch (FileNotFoundException fnf) {
+            System.out.println("File not found at " + file.getAbsolutePath());
+        } catch (InvalidClassException ic) {
+            System.out.println("Invalid class");
+        } catch (NotSerializableException ns) {
+            System.out.println("Not serializable");
+        } catch (IOException io) {
+            System.out.println(io.getMessage());
+        }
+        return false;
+    }
+
+    public static boolean loadBlockchain() {
+        try (FileInputStream fis = new FileInputStream(file);
+             BufferedInputStream bis = new BufferedInputStream(fis);
+             ObjectInputStream ois = new ObjectInputStream(bis)) {
+            blockchain = (Blockchain) ois.readObject();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
